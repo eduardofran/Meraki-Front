ea50d1e7c688a50f56c7c7fb53c49554018a0a36
Ollebacx <dav.ollebac@gmail.com>
2020-04-28 23:47:50 +0100

    Component Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,24 @@
 <template>
-  
-    <v-app-bar
-     >
-   
-           <v-tabs class="max"> 
-          <v-tab>Tab 1</v-tab>
-          <v-tab>Tab 2</v-tab>
-          <v-tab>Tab 3</v-tab>
-        </v-tabs>  
-      
-           <v-toolbar-title>Meraki</v-toolbar-title>
-     
-      <v-spacer></v-spacer>
-      <v-btn text >
-        Iniciar sesion
-      </v-btn>
-      <v-btn rounded outlined color="#298B7F">
-        Regístrate 
-      </v-btn>
-     
-     
-   
-      
-    </v-app-bar>
-    
- 
-  
+  <v-app-bar flat dense class="transparent">
+    <v-tabs class="max" small>
+      <v-tab>Inicio</v-tab>
+      <v-tab>Voluntariados</v-tab>
+      <!-- <v-tab>Favoritos</v-tab> -->
+    </v-tabs>
+
+
+    <v-toolbar-title>Meraki</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-btn text to="/login" small>Iniciar sesión</v-btn>
+    <v-btn to="/signup" rounded outlined small color="#298B7F">Regístrate</v-btn>
+  </v-app-bar>
 </template>
  <style lang="scss" scoped>
 .max {
   width: auto !important;
+}
+.transparent {
+  background-color:  !important;
 }
 </style>
