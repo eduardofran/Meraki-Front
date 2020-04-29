861e07d5f6e09d6b981a0ae846320c11a38ff149
Ollebacx <dav.ollebac@gmail.com>
2020-04-29 02:43:05 +0100

    Little chandes in Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,14 +7,20 @@
     </v-tabs>
 
 
-    <v-toolbar-title>Meraki</v-toolbar-title>
+    <router-link to="/"><h2 class="brand">Meraki</h2></router-link>
 
     <v-spacer></v-spacer>
-    <v-btn text to="/login" small>Iniciar sesión</v-btn>
+    <v-btn text to="/login" small class="mr-2">Iniciar sesión</v-btn>
     <v-btn to="/signup" rounded outlined small color="#298B7F">Regístrate</v-btn>
   </v-app-bar>
 </template>
  <style lang="scss" scoped>
+ *{
+   text-decoration-line: none;
+   .brand {
+     color: #298B7F;
+   }
+ }
 .max {
   width: auto !important;
 }
