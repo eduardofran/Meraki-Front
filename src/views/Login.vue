911877e8b8584afb4c01d7055d499feabd55f2c7
Ollebacx <dav.ollebac@gmail.com>
2020-04-29 02:44:00 +0100

    Login working

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,58 +1,78 @@
 <template>
-  <div>
-    <v-card width="400px" class="ml-5 mt-5 mx-a">
-      <v-card-title class="pb-0">
-        <h1 class="font-weight-thin">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="E-mail" v-model="email" :rules="emailRules" prepend-icon="mdi-email"></v-text-field>
-          <div id="err" v-if="userInvalid">Usuario no válido</div>
-          <v-text-field
-            label="Password"
-            v-model="userPassword"
-            :type="showPassword ? 'text' : 'password'"
-            prepend-icon="mdi-lock"
-            :rules="passwordRule"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-          ></v-text-field>
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn id="button" class="ma-3" color="amber" @click="login" >
-          Login
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+  <div class="bg">
+    <Navbar></Navbar>
+    <v-content>
+      <v-container class="justify-end">
+        <v-row class="justify-end">
+          <v-col>
+            <v-card
+              width="300px"
+              class="mt-12 mx-auto pa-2 text-center"
+              color="rgba(255, 255, 255, 0.75)"
+            >
+              <v-card-title class="pb-2">
+                <h2>Iniciar sesión</h2>
+              </v-card-title>
+              <v-card-text>
+                  <div id="err" v-if="userInvalid">Usuario no válido</div>
+                <v-form>
+                  <v-text-field label="E-mail" v-model="email" :rules="emailRules" background-color="#FAFAFA"></v-text-field>
+                  <v-text-field
+                    label="Contraseña"
+                    v-model="userPassword"
+                    :type="showPassword ? 'text' : 'password'"
+                    :rules="passwordRule"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword" background-color="#FAFAFA"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn id="button" color="#298B7F" dark block @click="login">Iniciar sesión</v-btn>
+              </v-card-actions>
+              <p class="caption">
+                ¿Aún no tienes cuenta?
+                <router-link to="/signup">Regístrate gratis aquí</router-link>
+              </p>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
 <script>
 import APIServices from "../services/Api";
+import Navbar from "../components/Navbar.vue";
+
 export default {
+  name: "Login",
   data() {
     return {
-      userInvalid:false,
+      userInvalid: false,
       showPassword: false,
       userPassword: "",
       passwordRule: [
-        v => !!v || "Password is required",
-        v => v.length >= 5 || "Password must be more than 5 characters"
+        v => !!v || "La contraseña es obligatoria",
+        v =>
+          v.length >= 5 || "La contraseña debe contener al menos 6 caracteres"
       ],
       email: "",
       emailRules: [
-        v => !!v || "E-mail is required",
-        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+        v => !!v || "El email es necesario",
+        v => /.+@.+\..+/.test(v) || "Formato de email incorrecto"
       ]
     };
+  },
+  components: {
+    Navbar
   },
   methods: {
     login() {
       const user = {
-        user_email: this.email,
-        user_password: this.userPassword
+        email: this.email,
+        password: this.userPassword
       };
       APIServices.login(user)
         .then(response => {
@@ -61,7 +81,7 @@ export default {
           } else {
             localStorage.setItem("token", response.token);
             // localStorage.setItem("id", response.id);
-            this.$router.push("/notes");
+            this.$router.push("/");
           }
         })
         .catch(err => console.log(err));
@@ -71,7 +91,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#button {
-  color: rgb(255, 255, 255);
+.bg {
+  background-image: url("https://images.unsplash.com/photo-1511632765486-a01980e01a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+  min-height: 100vh;
 }
 </style>
