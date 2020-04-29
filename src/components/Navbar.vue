5c0d2eea6181c90021e7442ad5d5183a36631c3c
eduardo <e630603002@hotmail.com>
2020-04-29 16:56:56 +0100

    home logo/navbar a medias

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-app-bar flat dense class="transparent">
-    <v-tabs class="max" small>
+  <v-app-bar  flat dense class="transparent">
+    <v-tabs class="max">
       <v-tab>Inicio</v-tab>
-      <v-tab>Voluntariados</v-tab>
+      <v-tab to="/">Voluntariados</v-tab>
       <!-- <v-tab>Favoritos</v-tab> -->
     </v-tabs>
 
@@ -12,6 +12,8 @@
     <v-spacer></v-spacer>
     <v-btn text to="/login" small class="mr-2">Iniciar sesión</v-btn>
     <v-btn to="/signup" rounded outlined small color="#298B7F">Regístrate</v-btn>
+    <v-btn  to="/" icon><v-icon>mdi-logout</v-icon></v-btn>
+
   </v-app-bar>
 </template>
  <style lang="scss" scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="home">
-   
+  <div>
+    <v-container>
+      <v-row>
+        
+          <v-card color='#7FCBAF' class="rounded"  >
+        <v-row>
+
+<v-col cols= '6'>
+  <v-card-title class="mt-12 ml-12 pt-12">
+   <h2>
+     Viaja. Explora. Ayuda.
+     </h2> 
+     
+  </v-card-title>
+  <v-card-subtitle class="mt-4 ml-12 fcolor">
+    <h2 >
+   Meraki es hacer algo con amor y creatividad, poniendo toda tu alma en ello.<br>
+<br>
+Aquí encontrarás la posibilidad de dar y recibir  ayuda de forma sencilla al mismo tiempo que  descubres todos los rincones del planeta.
+    </h2>
+
+
+  </v-card-subtitle>
+</v-col>
+<v-col cols='6' >
+  <span>
+  <v-img src='../assets/merakihome.png'></v-img>
+  </span>
+</v-col>
+        </v-row>
+  
+          </v-card>
+       
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -13,3 +46,16 @@ export default {
   
 }
 </script>
+<style lang="scss" scoped>
+* {
+  font-family: 'Quicksand', sans-serif;
+ 
+}
+.fcolor h2 {
+  color : white ;
+}
+.rounded {
+  border:solid 10px;
+  border-radius: 50px !important;
+}
+</style>
