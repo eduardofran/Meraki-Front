7abd00547862f7e9e12a7ac94c4340c725a0d577
eduardo <e630603002@hotmail.com>
2020-05-03 20:06:56 +0100

    componente de volver atras

---
+++ src/components/Back.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+<h4 @click="goBack" class= "mt-10 pointer">
+   < Volver atrás
+</h4>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    methods:{
+     goBack(){
+       window.history.back()
+     }
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.pointer{
+cursor: pointer;
+width: 110px;
+font-family: Quicksand, sans-serif;
+}
+</style>
