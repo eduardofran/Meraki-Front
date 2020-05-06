f8e6dff69fb7f9cd2478d088748d8902cb79f837
eduardo <e630603002@hotmail.com>
2020-05-06 17:43:43 +0100

    deployer

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.VUE_APP_API_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
